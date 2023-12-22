8dc96d925f65fd19c623252f6a5dada6657c7431
rheallyc <vireonxi@gmail.com>
2023-12-22 02:10:40 +0000

    Solve 1487C: Minimum Ties

---
+++ solutions/1487C_Minimum_Ties.cpp
@@ -0,0 +1,37 @@
+// Problem: Minimum Ties
+// URL: https://codeforces.com/problemset/problem/1487/C
+// Rating: 1500
+// Tags: brute force, constructive algorithms, dfs and similar, graphs, greedy, implementation, math
+// Language: C++17 (GCC 7-32)
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long  ll;
+int n;
+
+void solve(){
+    scanf("%d", &n);
+    if(n%2==1){
+        for(int i=1; i<=n*(n-1)/2; i++){
+            if(i%2==1) printf("1 ");
+            else printf("-1 ");
+        }
+        printf("\n");
+    }
+    else{
+        for(int i=n-1; i>=1; i--){
+            for(int j=1; j<=i; j++){
+                if(j==1 && i%2==1) printf("0 ");
+                else if(j%2==1) printf("1 ");
+                else printf("-1 ");
+            }
+        }
+        printf("\n");
+    }
+}
+
+int main(){
+    int t;
+    scanf("%d", &t);
+    while(t--) solve();
+    return 0;
+}
