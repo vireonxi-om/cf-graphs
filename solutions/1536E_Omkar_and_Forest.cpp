8ddfc827061a3ada89660d586b9aaa306af92819
rheallyc <vireonxi@gmail.com>
2024-03-12 18:17:08 +0000

    Solve 1536E: Omkar and Forest

---
+++ solutions/1536E_Omkar_and_Forest.cpp
@@ -0,0 +1,38 @@
+// Problem: Omkar and Forest
+// URL: https://codeforces.com/problemset/problem/1536/E
+// Rating: 2300
+// Tags: combinatorics, graphs, math, shortest paths
+// Language: C++17 (GCC 7-32)
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+int n, m;
+char a[2100][2100];
+ll ans[5100000];
+const ll M=1000000007;
+
+void solve(){
+    scanf("%d%d", &n, &m);
+    int cnt=0;
+    for(int i=1; i<=n; i++){
+        scanf("%s", a[i]);
+        for(int j=0; j<m; j++) if(a[i][j]=='#') cnt++;
+    }
+    ll result=ans[cnt];
+    if(cnt==n*m) result--;
+    printf("%lld\n", result);
+}
+
+int main(){
+    int t;
+    ans[0]=1;
+    for(int i=1; i<=4100000; i++){
+        ans[i]=2*ans[i-1];
+        ans[i]%=M;
+    }
+    scanf("%d", &t);
+    while(t--) solve();
+    return 0;
+}
+
+
