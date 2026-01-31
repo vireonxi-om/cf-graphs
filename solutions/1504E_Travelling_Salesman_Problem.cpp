9c8b5d4ede90dbd169c80e7a9a0520e8a189121a
rheallyc <vireonxi@gmail.com>
2026-01-31 16:59:23 +0000

    Solve 1504E: Travelling Salesman Problem

---
+++ solutions/1504E_Travelling_Salesman_Problem.cpp
@@ -0,0 +1,28 @@
+// Problem: Travelling Salesman Problem
+// URL: https://codeforces.com/problemset/problem/1504/E
+// Rating: 2200
+// Tags: binary search, data structures, dp, graphs, greedy, shortest paths, sortings
+// Language: C++17 (GCC 7-32)
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long  ll;
+int n;
+multiset<pair<int, int>> s;
+
+int main(){
+    scanf("%d", &n);
+    int a, c;
+    for(int i=1; i<=n; i++){
+        scanf("%d%d", &a, &c);
+        s.insert(make_pair(a, c));
+    }
+    int now=(*s.begin()).first;
+    ll ans=0;
+    for(auto x: s){
+        ans+=x.second;
+        if(now<x.first) ans+=x.first-now;
+        now=max(now, x.first+x.second);
+    }
+    printf("%lld\n", ans);
+    return 0;
+}
